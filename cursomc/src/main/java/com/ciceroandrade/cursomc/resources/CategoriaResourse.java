26ea32ce7e2409673d93a4acd6a196513c315174
Cicero Andrade <ciceroou@gmail.com>
2018-11-08 19:52:11 -0200

    teste classe dominio

--- cursomc/src/main/java/com/ciceroandrade/cursomc/resources/CategoriaResourse.java
+++ cursomc/src/main/java/com/ciceroandrade/cursomc/resources/CategoriaResourse.java
@@ -1,15 +1,29 @@
 package com.ciceroandrade.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ciceroandrade.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResourse {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List <Categoria> lista = new ArrayList<>();
+		lista.add(cat);
+		lista.add(cat2);
+		
+		
+		return lista;
 	}
 }
